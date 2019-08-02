4d46ebc5c6e01cc41eca29c9a5541bd98fd6271f
Tolgas1 <53622505+Tolgas1@users.noreply.github.com>
2019-08-02 21:13:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,19 +107,6 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-//istediğiniz yere ekleyin bot.js de
-antispam(client, {
-  uyarmaSınırı: 4, //Uyarılmadan önce aralıkta gönderilmesine izin verilen maksimum mesaj miktarı.
-  banlamaSınırı: 7, //Yasaklanmadan önce aralıkta gönderilmesine izin verilen maksimum ileti miktar.
-  aralık: 1000, // ms kullanıcılarda zaman miktarı, yasaklanmadan önce aralık değişkeninin maksimumunu gönderebilir.
-  uyarmaMesajı: "Spamı Durdur Yoksa Mutelerim.", // Uyarı mesajı, kullanıcıya hızlı gideceklerini belirten kullanıcıya gönderilir..
-  rolMesajı: "Spam için yasaklandı, başka biri var mı?", //Yasak mesaj, yasaklanmış kullanıcıyı ,Banlar
-  maxSpamUyarı: 8,//Bir kullanıcının uyarılmadan önce bir zaman dilimi içinde gönderebileceği maksimum kopya sayısı
-  maxSpamBan: 12, //Bir kullanıcının yasaklanmadan önce bir zaman diliminde gönderebildiği maksimum kopya sayısı
-  zaman: 7, // Spamdan sonraki zaman
-  rolİsimi: "spam-susturulmuş" // Spam Atan Kullanıcılar Verilecek Röl
-});
-
 
 
 client.login(process.env.BOT_TOKEN);
